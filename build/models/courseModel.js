e0ee0c0412a7d662be8b7590791f4793a494518d
Sharvari1515 <sharvarip715@gmail.com>
2026-01-08 21:17:51 +0530

    Add files via upload

---
+++ build/models/courseModel.js
@@ -0,0 +1,17 @@
+// models/courseModel.js
+const mongoose = require("mongoose");
+
+const courseSchema = new mongoose.Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  content: [
+    {
+      week: Number,
+      topic: String
+    }
+  ]
+}, { timestamps: true });
+
+const Course = mongoose.model("Course", courseSchema);
+
+module.exports = Course;
